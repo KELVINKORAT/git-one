5b01f801af75b4c11c41c6ecfb7110b5bb09da15
KELVINKORAT <kelvin.korat@marutitech.com>
2023-02-06 11:39:36 +0530

    Second Commit

--- FMS.py
+++ FMS.py
@@ -1,16 +1,18 @@
 import asyncio
 import random
 
+global count_1
+global count_2
+
+
 async def random_writer(string):
-    global count_1
     count_1 = 0
-    global count_2
     count_2 = 0
     for i in range(10):
-        pseudo_string_1=random.choice([string,'other'])
+        pseudo_string_1 = random.choice([string, 'other'])
         pseudo_string_2 = random.choice([string, 'other'])
         if pseudo_string_1 == 'MARUTI':
-            count_1=count_1+1
+            count_1 = count_1 + 1
         with open('File-1.txt', 'a') as f1:
             f1.write(pseudo_string_1+" ")
         await asyncio.sleep(1)
